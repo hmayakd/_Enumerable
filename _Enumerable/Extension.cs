df66129320f52252e6a3e017b65433a50ceb4a45
Hmayak Davtyan <davtyan.hmayak@gmail.com>
2022-06-01 17:40:58 +0400

    Add project files.

---
+++ _Enumerable/Extension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _Enumerable
+{
+    public static class _Extension
+    {
+        public static IEnumerable<int> _First(this IEnumerable<int> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+            foreach (var item in source)
+            {
+                yield return item;
+                yield break;
+            }
+        }
+        public static IEnumerable<int> _MinMax(this IEnumerable<int> source, int min)
+        {
+            foreach (var item in source)
+            {
+                if (item > min)
+                    yield return item;
+            }
+        }
+        public static IEnumerable<int> _MinMax(this IEnumerable<int> source, int min, int max)
+        {
+            foreach (var item in source)
+            {
+                if ((item > min) && (item < max))
+                    yield return item;
+            }
+        }
+        public static IEnumerable<int> _Add(this IEnumerable<int> source, int factor)
+        {
+            foreach (var item in source)
+            {
+                yield return item + factor;
+            }
+        }
+        public static IEnumerable<int> _Sub(this IEnumerable<int> source, int factor)
+        {
+            foreach (var item in source)
+            {
+                yield return item - factor;
+            }
+        }
+        public static IEnumerable<int> _Mul(this IEnumerable<int> source, int factor)
+        {
+            if (source == null) throw new ArgumentNullException();
+            foreach (var item in source)
+            {
+                yield return item * factor;
+            }
+        }
+        public static IEnumerable<int> _Div(this IEnumerable<int> source, int factor)
+        {
+            if (source == null) throw new ArgumentNullException();
+            foreach (var item in source)
+            {
+                yield return item / factor;
+            }
+        }
+    }
+}
---
+++ _Enumerable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Enumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> marks = new List<int> { -3, -2, -1, 0, 1, 2, 3 };
+            var newMarks = marks._First();
+            newMarks = marks._Add(2);
+            newMarks= marks._Sub(1);
+            newMarks = marks._Mul(10);
+            newMarks = newMarks._Div(3);
+            newMarks = newMarks._MinMax(3,10);
+        }
+    }
+}
